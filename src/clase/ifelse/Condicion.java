00987674529cdec4b2a0459d9908677f2ef86f94
TioCoding <tioCoding@gmail.com>
2016-09-19 02:36:16 -0500

    Usando operadores logicos de Java
    Pequeño ejemplo donde usamos los operadores logicos que soporta Java y como realizar pruebas de manera simple

--- src/clase/ifelse/Condicion.java
+++ src/clase/ifelse/Condicion.java
@@ -7,17 +7,37 @@ public class Condicion {
 	public void verificaNumero(int numero){
 		//if([condicion que sea verdad])
 		//else[se ejecutaria en caso de que no fuera verdad lo que esta dentro del if]
-		if(numero < 50){
-			System.out.println("El numero " + numero + ", es menor que 50");
+		
+		//<,<=,>,>=, &&,||, &,|, !, ==
+		
+		// && = (true)(true)(true) = true
+		// && = (true)(true)(true) ....... (false) = false
+		
+		// || = (true)(true)(true) = true;
+		// || = (true)(false)(false)....(false) = true;
+		
+		//OPERADOR AND (&&) Ejemplo
+		
+//		if(numero > 0 && numero < 50){
+//			System.out.println("El numero " + numero + ", esta entre 0 y 50");
+//		}else if(numero >50 && numero < 100){
+//			System.out.println("El numero " + numero + ", esta entre 50 y 100");
+//		}else{
+//			System.out.println("El numero " + numero + ", es mayor que 100");
+//		}
+		
+		//Operador OR (||) EJEMPLO
+		
+		if(numero > 50 || numero ==20 || numero == 30){
+			System.out.println("El numero "+ numero + ", a sido evaluado");
 		}else{
-			System.out.println("El numero " + numero + ", es mayor que 50");
+			System.out.println("El numero "+ numero + ", no se ejecuto en el if");	
 		}
 	}
 	
 	public static void main(String[] args){
 		Condicion condicion = new Condicion();
-		Calculadora calculadora = new Calculadora();
-		condicion.verificaNumero(calculadora.suma(20, 10));
+		condicion.verificaNumero(20);
 	}
 	
 }
